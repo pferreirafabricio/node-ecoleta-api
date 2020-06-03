6325ef18c302cc00d526649d53f03180ce18103c
Fabricio Pinto Ferreira <pferreirafabricio@gmail.com>
2020-06-02 23:41:21 -0300

    feat: add route for get all items

--- src/database/seeds/create_items.ts
+++ src/database/seeds/create_items.ts
@@ -3,7 +3,7 @@ import Knex from 'knex';
 export async function seed(knex: Knex) {
     await knex('items').insert([
         { title: "Lâmpadas", image: "lampadas.svg" },
-        { title: "Pilhas e Baterias", image: "baeterias.svg" },
+        { title: "Pilhas e Baterias", image: "baterias.svg" },
         { title: "Papéis e Papelão", image: "papeis-papelao.svg" },
         { title: "Resíduos e Eletrônicos", image: "eletronicos.svg" },
         { title: "Resíduos Orgânicos", image: "organicos.svg" },
--- src/routes.ts
+++ src/routes.ts
@@ -1,8 +1,19 @@
 import express from "express";
+import knex from "./database/connection";
+
 const routes = express.Router();
 
-routes.get("/", (request, response) => {
-  return response.status(400).json({ name: "Something" });
+routes.get("/items", async (request, response) => {
+  const items = await knex("items").select("*");
+
+  const serializedItems = items.map(item => {
+    return {
+      title: item.title,
+      image_url: `http://localhost:3333/uploads/${item.image}`
+    }
+  });
+
+  return response.json(serializedItems);
 });
 
 export default routes;
