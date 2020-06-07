be638aef2a9f4ff0ea5e6bbda360b26238f2b61f
Fabrício Pinto Ferreira <pferreirafabricio@gmail.com>
2020-06-07 10:59:48 -0300

    add upload images functionality and validation in create point router

--- src/controllers/PointsController.ts
+++ src/controllers/PointsController.ts
@@ -17,7 +17,14 @@ class PointsController {
 			.distinct()
 			.select("points.*");
 
-		return response.json(points);
+		const serializedPoints = points.map(point => {
+			return {
+				...point,
+				image_url: `http://192.168.1.101:3333/uploads/${point.image}`
+			}
+		});
+
+		return response.json(serializedPoints);
 	}
 
 	async show(request: Request, response: Response) {
@@ -28,13 +35,18 @@ class PointsController {
 		if (!point) {
 			return response.status(400).json({ message: "Point not found." });
 		}
-		console.log(id);
+
+		const serializedPoint = {
+			...point,
+			image_url: `http://192.168.1.101:3333/uploads/${point.image}`
+		}
+
 		const items = await knex("items")
 			.join("point_items", "items.id", "=", "point_items.item_id")
 			.where("point_items.point_id", id)
 			.select("items.title");
 
-		return response.json({ point, items });
+		return response.json({ point: serializedPoint, items });
 	}
 
 	async create(request: Request, response: Response) {
@@ -52,7 +64,7 @@ class PointsController {
 		const trx = await knex.transaction();
 
 		const point = {
-			image: "https://images.unsplash.com/photo-1488459716781-31db52582fe9?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=400&q=60",
+			image: request.file.filename,
 			name,
 			email,
 			whatsapp,
@@ -66,12 +78,15 @@ class PointsController {
 
 		const point_id = insertedIds[0];
 
-		const pointItems = items.map((item_id: number) => {
-			return {
-				item_id,
-				point_id
-			}
-		});
+		const pointItems = items
+			.split(",")
+			.map((item: string) => Number(item.trim()))
+			.map((item_id: number) => {
+				return {
+					item_id,
+					point_id
+				}
+			});
 
 		await trx("point_items").insert(pointItems);
 
--- src/server.ts
+++ src/server.ts
@@ -1,4 +1,5 @@
 import express from "express";
+import { errors } from "celebrate";
 import cors from "cors";
 import path from "path";
 import routes from "./routes";
@@ -9,5 +10,7 @@ app.use(express.json());
 app.use(routes);
 
 app.use("/uploads", express.static(path.resolve(__dirname, "..", "uploads")));
+
+app.use(errors());
 
 app.listen(3333);
